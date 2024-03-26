e5e4657619d0ce3ece5d863dc0022e2461d4f443
Nastia Felt <anastasia.felt@gmail.com>
2024-03-26 17:12:43 +0000

    handle conditions * added for loop * added tshirt and jumper column

--- python_assessment/taps_aff.py
+++ python_assessment/taps_aff.py
@@ -1,55 +1,49 @@
 # Given a list of locations and temperatures:
-    # create field (what_to_wear)
-    # if temp < 15C then add 'jumper'
-    # if temp > OR == 15C then add 'tshirt'
-    # write a script that reads a csv
-    # fills in field (what_to_wear)
-    # publishes a new file
-    # copy whole file into output
-    # add heading field
-    # populate
+# create field (what_to_wear)
+# if temp < 15C then add 'jumper'
+# if temp > OR == 15C then add 'tshirt'
+# write a script that reads a csv
+# fills in field (what_to_wear)
+# publishes a new file
+# copy whole file into output
+# add heading field
+# populate
 
 import csv
 
-# reference both files 
+# reference both files
 input_path = 'input.csv'
 output_path = 'output.csv'
+base_temperature = 15
 
 
-with open (input_path, 'r') as input_file:
-        
-       # create csv reader object & parse contents of the file
-        reader = csv.reader(input_file)
+with open(input_path, 'r') as input_file:
 
-        # store contents as list of lists
-        rows = list(reader)
-    
-        # get the headers and add "what_to_wear"
-        rows[0].append('what_to_wear')
-            
+    # create csv reader object & parse contents of the file
+    reader = csv.reader(input_file)
+
+    # store contents as list of lists
+    rows = list(reader)
+
+    # get the headers and add "what_to_wear"
+    rows[0].append('what_to_wear')
+
+    # access rows from 1-end
+    # add conditional statements
+    # create new column
+    for row in rows[1:]:
+        if int(row[1]) < base_temperature:
+            row.append('jumper')
+
+        elif int(row[1]) >= base_temperature:
+            row.append('tshirt')
+
 
 # add the contents with the new data to the new csv
 with open(output_path, 'w') as output_file:
 
-    # create csv writer object 
+    # create csv writer object
     writer = csv.writer(output_file)
 
     # write all rows to output_file
     writer.writerows(rows)
-  
-
-         # foo.split(',')
-            
-
-
-
-        
-
-            
-
-
-
-
-    
-
-
